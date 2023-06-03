546996ed85ebbd5e7862322922f42d4f3f041c2e
Farhad Jems <farhadjems@gmail.com>
2023-06-03 16:31:42 +0330

    fixed the message

--- chapter02/ch2p1.cpp
+++ chapter02/ch2p1.cpp
@@ -4,5 +4,5 @@ using namespace std;
 
 int main() // main function
 {
-    cout << " Hello , my name is farhad\n"; // print command - printing name
+    cout << " Hello , my name is Farhad\n"; // print command - printing name
 }
